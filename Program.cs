b879ea0b4663cba4c69ba3c985c7462e80b3eec0
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-24 14:17:15 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Base;
+            double Altura;
+            double Area;
+
+            Console.WriteLine("digite o valor da base (cm):");
+            Base = double.Parse(Console.ReadLine ());
+            Console.ResetColor();
+
+            Console.Write("Digite o Valor da Altura (cm): ");
+            Altura = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.WriteLine($"A Área do Triangulo é: {Area = Base * Altura / 2} (cm²)");
+            Console.ResetColor();    
+               //---fim(digite)---//
+            
+               //---inicio(sair)---//
+            Console.WriteLine("pressione Enter para sair!"); 
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
